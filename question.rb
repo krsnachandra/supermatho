eb45b5823a51c40f78b9385cf15c02dd3aae0c1f
Krsna Chandra Musterer <chandra@Krsnas-MacBook-Air.local>
2018-01-01 15:03:26 -0800

    first commit

---
+++ question.rb
@@ -0,0 +1,11 @@
+class Question
+
+  attr_accessor :ans
+
+  def initialize
+    num1 = 1 + rand(100)
+    num2 = 1 + rand(100)
+    @ans = num1 + num2
+    puts "What is #{num1} + #{num2}?"
+  end
+end
